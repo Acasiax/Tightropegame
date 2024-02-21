ee0981f1730238d9b28bf43a2e06153a1b32f007
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-02-21 21:58:12 +0900

    Initial Commit

---
+++ Tightropegame/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  Tightropegame
+//
+//  Created by 이윤지 on 2/21/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
